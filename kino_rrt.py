2da64cd85c612a5a8e3ee143aaaf20042941d47a
Sina-Haz <101819919+Sina-Haz@users.noreply.github.com>
2024-10-23 17:25:18 -0400

    KRRT skeleton

--- kino_rrt.py
+++ kino_rrt.py
@@ -1,1 +1,36 @@
-f
+# Tree data structure -> Holds states, each state can be a "Node" in the tree
+import numpy as np
+import mujoco
+
+
+xml = 'nav1.xml'
+
+class Node:
+    def __init__(self, q,  qdot, parent) -> None:
+        self.q = q
+        self.qdot = qdot
+        self.parent = parent
+        self.ctrl = None
+
+
+class KRRT:
+    def __init__(self, start: Node, goal: Node) -> None:
+        self.Tree = [start]
+        self.goal = goal
+        self.t = 0
+        self.model = mujoco.MjModel.from_xml_path(xml)
+        self.data = mujoco.MjData(self.model)
+
+    def sample_cfg(self)->np.array:
+        pass
+
+    def nearest_neighbor(self, x: Node)->Node:
+        pass
+
+    def sample_ctrl(self)->np.array:
+        pass
+
+    def simulate_ctrl(self, nearest_idx, ctrl)->bool:
+        pass
+
+    
